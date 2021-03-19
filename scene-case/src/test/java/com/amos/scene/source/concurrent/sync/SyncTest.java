8ce276ad55bee2cb18466f295daf8022c71d71ef
amos.wang <amos.wang@xiaoi.com>
2021-03-19 11:53:40 +0800

    feat: jol print object size

--- scene-case/src/test/java/com/amos/scene/source/concurrent/sync/SyncTest.java
+++ scene-case/src/test/java/com/amos/scene/source/concurrent/sync/SyncTest.java
@@ -2,6 +2,7 @@ package com.amos.scene.source.concurrent.sync;
 
 import org.junit.jupiter.api.Test;
 import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.info.GraphLayout;
 
 /**
  * Synchronized Test
@@ -21,7 +22,6 @@ public class SyncTest {
 
     /**
      * 内存布局分为3个区域：对象头（Header）、实例数据（Instance Data）和对齐填充（Padding）
-     * {@link <url>https://www.cnblogs.com/LemonFive/p/11246086.html</url>}
      * <p>
      * OFFSET  SIZE     TYPE DESCRIPTION       VALUE
      * 0       4          (object header)      05 00 00 00 (00000101 00000000 00000000 00000000) (5)         MarkWord
@@ -35,6 +35,8 @@ public class SyncTest {
      * <p>
      * 锁标识位：01-未锁定 00-轻量级锁定 10-膨胀（重量级锁定） 11-GC标记 01-可偏向
      * 也即第一行中的，第一个串 00000101
+     *
+     * @see <a href="https://www.cnblogs.com/LemonFive/p/11246086.html">从打印Java对象头说起</a>
      */
     @Test
     public void printMarkWord() {
@@ -45,6 +47,12 @@ public class SyncTest {
         System.out.println(ClassLayout.parseInstance(name).toPrintable());
         System.out.println(ClassLayout.parseInstance(age).toPrintable());
         System.out.println(ClassLayout.parseInstance(sex).toPrintable());
+    }
+
+    @Test
+    public void objectSize() {
+        String name = "88888888";
+        System.out.println(GraphLayout.parseInstance(name).totalSize());
     }
 
 }
